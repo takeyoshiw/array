4442d87c963ce434c572bc3ae98cf0e3778a02aa
Takashi Imagire <imagire@gmail.com>
2018-08-03 10:55:12 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-// argv[1] に問題のファイル名が入ります
 int main(int argc, char *argv[]) 
 {
-    FILE *fp;
-
-    // ファイルの読み込み
-    char *filename = argv[1];
-    if ((fp = fopen(filename, "r")) == NULL) {
-        fprintf(stderr, "Failed to open %s.\n", filename);
-        exit(EXIT_FAILURE);
+    char str[5];
+    int array[100];
+    int index = 0;
+    while(fgets(str, sizeof(str), stdin)){
+        int n = atoi(str);
+        if(0 <= n){
+            array[index++] = n;
+        }else{
+            printf("%d\n", array[-n-1]);
+        }
     }
-
-    //##### ここから書き換えてファイルを読み込んで、結果を出力する
-    char str[1000];
-    fgets(str, sizeof(str), fp);
-    printf("%s", str);
-
-    //##### だいたいここまで書き換える
-
-    // ファイルを閉じます
-    fclose(fp);
 
     return 0;
 }
