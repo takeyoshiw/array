51f9a0cf96aedc5fa4147bc0df8f48f26a605385
Takashi Imagire <imagire@gmail.com>
2018-08-03 10:59:36 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,15 +3,9 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
-    int array[100];
-    int index = 0;
+    
     while(fgets(str, sizeof(str), stdin)){
-        int n = atoi(str);
-        if(0 <= n){
-            array[index++] = n;
-        }else{
-            printf("%d\n", array[-n-1]);
-        }
+        printf("%s\n", str);
     }
 
     return 0;
