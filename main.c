544468ea6a8bfbd28498acc1bb320fb6a73db8b8
takeyoshiw <43458963+takeyoshiw@users.noreply.github.com>
2018-09-26 19:51:46 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main() 
+int main(int n) 
 {
-    int array[];
+    int array[100];
+    int i;
+    for(i=0;i<100;i++){
+        if(n>=0)
+           array[i]=n;
+        else if(n<0)
+            printf("%d",array[n])}
     
     return 0;
 }
