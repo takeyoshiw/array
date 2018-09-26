2fe3be4448788985080077c7f87c51e8af8cc032
takeyoshiw <43458963+takeyoshiw@users.noreply.github.com>
2018-09-26 20:37:25 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,14 +5,12 @@ int main(int n)
     int array[100];
     int i=0;
     
-    while(n>100){
+    
       if(n<0)
         printf("%d\n",array[n])
     else if(n>=0){
         array[i]=n;
         i++;
-    }else
-        break;
     }
     
     return 0;
