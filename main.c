e59be9d5c67c9e3535c6e0f5950f256595da95fb
takeyoshiw <43458963+takeyoshiw@users.noreply.github.com>
2018-09-26 20:06:39 +0900

    Update main.c

--- main.c
+++ main.c
@@ -10,3 +10,15 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+int main(int n)
+{
+    int array[100],i;
+    
+    for(i=0;i<100;i++){
+        if(n>=0)
+            array[i]=n;
+        else
+            printf("%d\n",array[n])
+    }
+    return 0;
+}
