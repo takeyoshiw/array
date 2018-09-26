ae45f1177345d680d1730b5eb4298ed23147bca9
takeyoshiw <43458963+takeyoshiw@users.noreply.github.com>
2018-09-26 20:41:17 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main(int n)
+int main(int argc, char *argv[])
 {
     int array[100];
     int i=0;
-    
+    int n;
     
       if(n<0)
-        printf("%d\n",array[n])
+        printf("%d\n",array[n]);
     else if(n>=0){
         array[i]=n;
         i++;
